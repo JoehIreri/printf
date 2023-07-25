3fda86f867ab2e1591008f18f6c4fb3409484354
PangoClemo <133219718+PangoClemo@users.noreply.github.com>
2023-07-25 22:27:51 +0300

    Update precision.c

--- precision.c
+++ precision.c
@@ -2,7 +2,7 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * by Joeh and Clement
+ * by Joeh and Clement pango
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
