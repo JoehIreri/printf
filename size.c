3f6e6f70ef9b1fc42e8de726cb65a835e715c274
PangoClemo <133219718+PangoClemo@users.noreply.github.com>
2023-07-25 22:31:16 +0300

    Update size.c

--- size.c
+++ size.c
@@ -2,7 +2,7 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * by Joeh and Clement
+ * by Joeh and Clement pango
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
