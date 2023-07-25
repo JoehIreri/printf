4e55670295aaf7af139d8620dc589ce3c3e6b7e3
PangoClemo <133219718+PangoClemo@users.noreply.github.com>
2023-07-25 22:32:01 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -2,7 +2,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * by Joeh and Clement
+ * by Joeh and Clement pango
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
